47e259b45effbab305901645d40a899ca550d939
Tobias Lindgaard <tobias.private@gmail.com>
2011-12-21 21:45:53 +0100

    started solving equations

--- equation.py
+++ equation.py
@@ -2,14 +2,29 @@ import math
 
 def main():
         print("Main")
+        solve()
 
-def solveE():
+def solve():
         se = raw_input('Please enter your equation > ')
+        a=0
+        b=[]
+        while a < len(se):
+                b.append(se[a])
+                a=a+1
+                print(b)
+
         
+        #To figure out where our '=' is in the list
+        #This is probably gonna be very unestaestetical.
+        #As I think it will rely a good deal on if statements.
 
-
-
-
+        #a=0
+        #if list[a]=='=':
+        #       workinglist=list[:a] #getting all the part of list up till a
+        #
+        #Here we are going to do some code to calculate what the parts of the math means
+        
+        
 
 
 
